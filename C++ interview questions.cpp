0eb9b451fb03d91ed4bb5162c2ae6a6b2b87f5c7
Christopher Morgan <60473703+chrislouis86@users.noreply.github.com>
2024-04-17 23:04:21 +0100

    Update C++ interview questions.cpp

--- C++ interview questions.cpp
+++ C++ interview questions.cpp
@@ -1,18 +1,17 @@
 // C++ interview questions.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-using std
+using namespace std; 
 
 
-// 1. Check Program to check if wether a number is negative or positive
+// 1. Write a program to check if wether a number is negative or positive. 
 int main()
 {
 	int number;
 	number = 100;
 	if (number >= 0)
 	{
-		std::cout << " is a postivive number. " << std::endl;
+		std::cout << " is a positive number. " << std::endl;
 	}
 	else  
 	{
